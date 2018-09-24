e91a56a793a21ccf80e69520a99caf26eb903a8d
Mario <alespcs@gmail.com>
2018-09-24 13:06:26 -0400

    add server.js for deploy heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/sdg_test'));
+app.use(express.static(__dirname + '/dist/sdg-test'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/sdg_test/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/sdg-test/index.html'));
 });
 
 // default Heroku port
